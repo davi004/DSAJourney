3c55a7e180a30a08761fc7199c5eb975b70f4bf4
davi <davi@davi-Inspiron-3542>
2018-03-21 16:41:59 +0530

    new changes

--- src/com/davi/open/HelloServlet.java
+++ src/com/davi/open/HelloServlet.java
@@ -43,7 +43,7 @@ public class HelloServlet extends HttpServlet {
 		writer.println("<h1>Hello " + yourName + "</h1>");
 		writer.close();
 		
-		
+//add the comment to check the commit		
 		doGet(request, response);
 	}
 
